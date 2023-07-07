d97155f1664af8c4b500ee6f222111100683d008
lockround <websbiz0000@gmail.com>
2023-07-07 17:45:13 +0000

    fix issue in captcha

--- main.tsx
+++ main.tsx
@@ -49,10 +49,12 @@ router.get("/tiktok", async (ctx) => {
     const page = await browser.newPage();
     await page.goto('https://www.tiktok.com/@eyeinspired/video/7252706573519310122', { waitUntil: "domcontentloaded" })
   // await page.waitForTimeout(5000)
-  // await page.click('[data-e2e=modal-close-inner-button]')
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-let comments = {}
   let $ = cheerio.load(data);
+  if($('[data-e2e=modal-close-inner-button]')){
+    await page.click('[data-e2e=modal-close-inner-button]')
+  }
+let comments = {}
   $('[data-e2e=comment-username-1]').map((i,el) => {
     let username = $(el).text()
     comments[i] = {username}
