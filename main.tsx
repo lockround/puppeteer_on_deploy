93963d374fcb2bf3cbd78bec39a81c2963318bc9
lockround <websbiz0000@gmail.com>
2023-07-07 22:12:34 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -47,8 +47,8 @@ router.get("/tiktok", async (ctx) => {
   });
   try {
     const page = await browser.newPage();
-    await page.goto('https://www.tiktok.com/@eyeinspired/video/7252706573519310122')
-  await page.waitForTimeout(5000)
+    await page.goto('https://www.tiktok.com/@eyeinspired/video/7252706573519310122',, { waitUntil: "domcontentloaded" })
+  // await page.waitForTimeout(5000)
   await page.click('[data-e2e=modal-close-inner-button]')
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 let comments = {}
